94286bd339b5eca78046f1575d2de1f6ba48b3e0
Afif Fadhlurrahman <afif.fadhlurrahman2@gmail.com>
2024-03-30 23:23:18 +0700

    Create Order History Page

---
+++ noQ/OrderHistory.swift
@@ -0,0 +1,18 @@
+//
+//  OrderHistory.swift
+//  noQ
+//
+//  Created by Muhammad Afif Fadhlurrahman on 30/03/24.
+//
+
+import SwiftUI
+
+struct OrderHistory: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderHistory()
+}
